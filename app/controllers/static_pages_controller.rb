52c906c60e54cdf0aa3954d95501f95aa42ad6a7
Matthew Zagaja <mzagaja@alum.wpi.edu>
2015-06-07 14:19:40 -0400

    Legislator lookup using HTML5 location test.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,16 @@
+require 'open-uri'
 class StaticPagesController < ApplicationController
   def lookup
   end
 
-  def help
+  def contact
+    location = params[:location].split(",")
+    base_url = 'http://openstates.org/api/v1//legislators/geo/'
+    lat = '?lat=' + location[0]
+    long = '&long=' + location[1]
+    api_key = '&apikey=' + '7df140cf73d0488897c53b2039f035b8'
+    full_url = base_url + lat + long + api_key
+    @legislator = JSON.parse(open(full_url).read)
+    @test = location[0] + "," + location[1]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'static_pages/lookup'
 
-  get 'static_pages/help'
+  get 'static_pages/contact'
 
   root 'static_pages#lookup'
 
